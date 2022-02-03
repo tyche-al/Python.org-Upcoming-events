a9ff74844d8d8342dd7ecfceca969f664c1fed4d
Anastasia <40719581+tyche-al@users.noreply.github.com>
2022-02-03 22:09:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,9 @@ EVENT_LINK = './a'
 
 class Event:
 	def __init__(self, event):	
-		self.event = event
-
-	def parse(self):
-		self.event_date = datetime.fromisoformat(self.event.xpath(TIME)[0].get('datetime'))
-		self.event_name = (self.event.xpath(EVENT_LINK)[0].text_content())
-		self.event_info = (self.event.xpath(EVENT_LINK)[0].get('href'))
+		self.event_date = datetime.fromisoformat(event.xpath(TIME)[0].get('datetime'))
+		self.event_name = (event.xpath(EVENT_LINK)[0].text_content())
+		self.event_info = (event.xpath(EVENT_LINK)[0].get('href'))
 
 	def __str__(self):
 		return f'{self.event_date} {self.event_name} {SOURCE}{self.event_info}'
@@ -34,5 +31,4 @@ def get_events():
 if __name__ == '__main__':
 	for item in get_events():
 		event = Event(item)
-		event.parse()
 		print(event)
